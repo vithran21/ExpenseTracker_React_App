547d8b25dae2fc01892659cf7ebae7dae20c89cb
vithran21 <56606871+vithran21@users.noreply.github.com>
2024-10-08 23:38:14 +0530

    Segregated Expenses further to ExpensesList Cponent

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
+import ExpensesList from "./ExpensesList";
 import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
@@ -15,26 +15,13 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === filteredYear
   })
 
-  let expensesContent = <p>No expenses found.</p>
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ))
-  }
-
   return (
     <Card className="expenses">
-      <ExpensesFilter
+      <ExpensesFilter 
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {expensesContent}
+      <ExpensesList items={filteredExpenses}/>
     </Card>
   );
 }
